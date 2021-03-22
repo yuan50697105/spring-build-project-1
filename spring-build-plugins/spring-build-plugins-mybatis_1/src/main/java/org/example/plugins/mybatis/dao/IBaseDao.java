a0d363af7e700226e722e023c5f039c0740cc69f
yuan50697105 <710575900@qq.com>
2021-03-22 15:52:42 +0800

    [新增功能](master): 初始提交

--- spring-build-plugins/spring-build-plugins-mybatis_1/src/main/java/org/example/plugins/mybatis/dao/IBaseDao.java
+++ spring-build-plugins/spring-build-plugins-mybatis_1/src/main/java/org/example/plugins/mybatis/dao/IBaseDao.java
@@ -2,9 +2,10 @@ package org.example.plugins.mybatis.dao;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.gitee.denger.mybatis.example.ext.MybatisExample;
+import org.example.plugins.mybatis.entity.po.IBaseEntity;
 
 /**
  * @author yuane
  */
-public interface IBaseDao<T, Q extends MybatisExample<E>, E> extends IService<T> {
+public interface IBaseDao<T extends IBaseEntity, Q extends MybatisExample<E>, E> extends IService<T> {
 }
--- spring-build-plugins/spring-build-plugins-mybatis_1/src/main/java/org/example/plugins/mybatis/dao/impl/IBaseDaoImpl.java
+++ spring-build-plugins/spring-build-plugins-mybatis_1/src/main/java/org/example/plugins/mybatis/dao/impl/IBaseDaoImpl.java
@@ -3,10 +3,11 @@ package org.example.plugins.mybatis.dao.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gitee.denger.mybatis.example.ext.MybatisExample;
 import org.example.plugins.mybatis.dao.IBaseDao;
+import org.example.plugins.mybatis.entity.po.IBaseEntity;
 import org.example.plugins.mybatis.mapper.IBaseMapper;
 
 /**
  * @author yuane
  */
-public abstract class IBaseDaoImpl<T, Q extends MybatisExample<E>, E, M extends IBaseMapper<T>> extends ServiceImpl<M, T> implements IBaseDao<T, Q, E> {
+public abstract class IBaseDaoImpl<T extends IBaseEntity, Q extends MybatisExample<E>, E, M extends IBaseMapper<T>> extends ServiceImpl<M, T> implements IBaseDao<T, Q, E> {
 }
