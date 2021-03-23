a9a1db66dd6ea919d2c70298e7dba44dd21f2b15
yuan50697105 <710575900@qq.com>
2021-03-23 12:08:45 +0800

    [新增功能](master): 初始提交

--- spring-build-plugins/spring-build-plugins-oss-tencent/src/main/java/org/example/plugins/oss/tencent/properties/COSClientProperties.java
+++
@@ -1,7 +0,0 @@
-package org.example.plugins.oss.tencent.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "oss.tencent")
-public class COSClientProperties {
-}
