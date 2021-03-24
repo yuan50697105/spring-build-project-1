d904a852be4be5f30c7663b5684a24d6050ef0a3
yuan50697105 <710575900@qq.com>
2021-03-24 10:28:51 +0800

    [新增功能](master): 初始提交

--- spring-build-domains/spring-build-domains-service-account_2/src/main/java/org/example/domains/AccountServiceConfiguration.java
+++ spring-build-domains/spring-build-domains-service-account_2/src/main/java/org/example/domains/AccountServiceConfiguration.java
@@ -1,4 +1,4 @@
-package org.example.domains.configuration;
+package org.example.domains;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
