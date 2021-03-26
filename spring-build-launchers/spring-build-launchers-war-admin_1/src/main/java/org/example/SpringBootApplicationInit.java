aeb90dcd1219d7b3dd7c123ecdfd852a91a9776a
yuan50697105 <710575900@qq.com>
2021-03-26 16:12:28 +0800

    [新增功能](develop):

--- spring-build-launchers/spring-build-launchers-war-admin_1/src/main/java/org/example/SpringBootApplicationInit.java
+++ spring-build-launchers/spring-build-launchers-war-admin_1/src/main/java/org/example/SpringBootApplicationInit.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableKnife4j
 public class SpringBootApplicationInit extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
