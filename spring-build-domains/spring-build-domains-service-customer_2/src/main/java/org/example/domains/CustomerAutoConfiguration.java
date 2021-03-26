a8a2bf9f515ee52d99f34e359f98c993f9a41b2c
yuan50697105 <710575900@qq.com>
2021-03-26 16:54:44 +0800

    [新增功能](develop):

--- spring-build-domains/spring-build-domains-service-customer_2/src/main/java/org/example/domains/CustomerAutoConfiguration.java
+++ spring-build-domains/spring-build-domains-service-customer_2/src/main/java/org/example/domains/CustomerAutoConfiguration.java
@@ -5,5 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-public class CustomerInfoAutoConfiguration {
+public class CustomerAutoConfiguration {
 }
