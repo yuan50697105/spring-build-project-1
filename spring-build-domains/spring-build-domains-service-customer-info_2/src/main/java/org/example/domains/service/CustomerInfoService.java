c34dbeb9ac8cdf3fc172a9d7082cea71ede8352a
yuan50697105 <710575900@qq.com>
2021-03-26 14:30:01 +0800

    [新增功能](develop):

--- spring-build-domains/spring-build-domains-service-customer-info_2/src/main/java/org/example/domains/service/CustomerInfoService.java
+++ spring-build-domains/spring-build-domains-service-customer-info_2/src/main/java/org/example/domains/service/CustomerInfoService.java
@@ -1,7 +1,22 @@
 package org.example.domains.service;
 
+import org.example.modules.repository.mysql.entity.query.CustomerInfoQuery;
+import org.example.modules.repository.mysql.entity.vo.CustomerInfoFormVo;
 import org.example.modules.repository.mysql.entity.vo.CustomerInfoVo;
+import org.example.plugins.mybatis.entity.IPageData;
+
+import java.util.List;
 
 public interface CustomerInfoService {
-    void save(CustomerInfoVo customerInfoVo);
+    IPageData<CustomerInfoVo> page(CustomerInfoQuery query);
+
+    List<CustomerInfoVo> list(CustomerInfoQuery query);
+
+    void save(CustomerInfoFormVo customerInfoFormVo);
+
+    void update1(Long id, CustomerInfoFormVo customerInfoFormVo);
+
+    void update2(CustomerInfoFormVo customerInfoFormVo);
+
+    CustomerInfoVo get(Long id);
 }
--- spring-build-domains/spring-build-domains-service-customer_1/src/main/java/org/example/domains/service/impl/CustomerServiceImpl.java
+++ spring-build-domains/spring-build-domains-service-customer_1/src/main/java/org/example/domains/service/impl/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.domains.entity.vo.CustomerVo;
 import org.example.domains.service.CustomerService;
 import org.example.modules.repository.mysql.entity.vo.CustomerContractVo;
 import org.example.modules.repository.mysql.entity.vo.CustomerInfoFormVo;
+import org.example.modules.repository.mysql.entity.vo.CustomerInfoVo;
 import org.example.modules.repository.mysql.repository.CustomerContractRepository;
 import org.example.modules.repository.mysql.repository.CustomerInfoRepository;
 import org.springframework.stereotype.Service;
@@ -23,12 +24,12 @@ public class CustomerServiceImpl implements CustomerService {
     public void save(CustomerVo vo) {
         CustomerInfoFormVo customer = vo.getCustomer();
         Long customerId = customerInfoRepository.saveWithId(customer);
-        Optional<CustomerInfoFormVo> optional = customerInfoRepository.getByIdOpt(customerId);
+        Optional<CustomerInfoVo> optional = customerInfoRepository.getByIdOpt(customerId);
         if (optional.isPresent()) {
-            CustomerInfoFormVo customerInfoFormVo = optional.get();
+            CustomerInfoVo customerInfoVo = optional.get();
             CustomerContractVo contract = vo.getContract();
-            contract.setCustomerId(customerInfoFormVo.getId());
-            contract.setCustomerCode(customerInfoFormVo.getCustomerCode());
+            contract.setCustomerId(customerInfoVo.getId());
+            contract.setCustomerCode(customerInfoVo.getCode());
             customerContractRepository.saveWithId(contract);
         }
     }
