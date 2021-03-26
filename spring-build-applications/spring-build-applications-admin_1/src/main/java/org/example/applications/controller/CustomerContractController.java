69063e8c152b312548fb3e6a13c278c5f66f0f62
yuan50697105 <710575900@qq.com>
2021-03-26 15:23:18 +0800

    [新增功能](develop):

--- spring-build-applications/spring-build-applications-admin_1/src/main/java/org/example/applications/controller/CustomerContractController.java
+++ spring-build-applications/spring-build-applications-admin_1/src/main/java/org/example/applications/controller/CustomerContractController.java
@@ -5,6 +5,9 @@ import ai.yue.library.base.view.Result;
 import lombok.AllArgsConstructor;
 import org.example.domains.service.CustomerContractService;
 import org.example.modules.repository.mysql.entity.vo.CustomerContractVo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerContractController {
     private final CustomerContractService customerContractService;
 
-    public Result<?> save(CustomerContractVo customerContractVo) {
+    @PostMapping
+    public Result<?> save(@RequestBody @Validated CustomerContractVo customerContractVo) {
         customerContractService.save(customerContractVo);
         return R.success();
     }
