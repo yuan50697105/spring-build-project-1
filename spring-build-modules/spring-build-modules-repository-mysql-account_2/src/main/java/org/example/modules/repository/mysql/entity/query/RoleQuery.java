07e4f4739ead16479855db0204ff37d743be88bc
yuan50697105 <710575900@qq.com>
2021-03-27 11:13:41 +0800

    [新增功能](develop):

--- spring-build-modules/spring-build-modules-repository-mysql-account_2/src/main/java/org/example/modules/repository/mysql/entity/query/RoleQuery.java
+++ spring-build-modules/spring-build-modules-repository-mysql-account_2/src/main/java/org/example/modules/repository/mysql/entity/query/RoleQuery.java
@@ -3,11 +3,12 @@ package org.example.modules.repository.mysql.entity.query;
 import com.gitee.denger.mybatis.example.ext.annotation.AndLike;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.example.plugins.commons.entity.query.BaseQuery;
 import org.example.plugins.mybatis.entity.query.IBaseQuery;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class RoleQuery extends IBaseQuery {
+public class RoleQuery extends BaseQuery {
     @AndLike
     private String name;
 
