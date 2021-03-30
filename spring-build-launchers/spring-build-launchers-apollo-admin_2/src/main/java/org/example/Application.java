123be69b662d8952469f69374d6fdfa48e3c9912
yuan50697105 <710575900@qq.com>
2021-03-30 20:50:39 +0800

    [新增功能](develop):

---
+++ spring-build-launchers/spring-build-launchers-apollo-admin_2/src/main/java/org/example/Application.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@EnableKnife4j
+@EnableSwagger2WebMvc
+@EnableSpringUtil
+@SpringBootApplication(scanBasePackages = {"org.example"})
+@EnableApolloConfig
+public class Application {
+    @Autowired
+    private ApplicationContext context;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            String[] beanDefinitionNames = context.getBeanNamesForAnnotation(Controller.class);
+            ArrayList<String> strings = new ArrayList<>(Arrays.asList(beanDefinitionNames));
+            for (String string : strings) {
+                System.out.println("beanName = " + string);
+            }
+
+        };
+    }
+
+}
