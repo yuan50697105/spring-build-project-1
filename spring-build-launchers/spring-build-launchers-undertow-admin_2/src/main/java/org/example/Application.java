2a9714e5d93f9b43596f83b6e4bc62e0f17fab04
yuan50697105 <710575900@qq.com>
2021-03-30 13:31:16 +0800

    [新增功能](develop):

--- spring-build-launchers/spring-build-launchers-undertow-admin_2/src/main/java/org/example/Application.java
+++ spring-build-launchers/spring-build-launchers-undertow-admin_2/src/main/java/org/example/Application.java
@@ -1,16 +1,13 @@
 package org.example;
 
 import cn.hutool.extra.spring.EnableSpringUtil;
-import cn.hutool.extra.spring.SpringUtil;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import org.example.applications.AdminAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
@@ -21,12 +18,12 @@ import java.util.Arrays;
 @EnableSwagger2WebMvc
 @EnableSpringUtil
 @SpringBootApplication(scanBasePackages = {"org.example"})
-public class SpringBootApplication1 {
+public class Application {
     @Autowired
     private ApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApplication1.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 
