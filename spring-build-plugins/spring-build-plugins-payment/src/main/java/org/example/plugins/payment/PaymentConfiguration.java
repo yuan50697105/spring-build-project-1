1d9d07820c25f9915cffb094eb6a12b392225706
yuan50697105 <710575900@qq.com>
2021-03-30 12:46:31 +0800

    [新增功能](develop):

--- spring-build-plugins/spring-build-plugins-payment/src/main/java/org/example/plugins/payment/PaymentConfiguration.java
+++ spring-build-plugins/spring-build-plugins-payment/src/main/java/org/example/plugins/payment/PaymentConfiguration.java
@@ -13,10 +13,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(AlipayProperties.class)
+@ComponentScan
 public class PaymentConfiguration {
     @Autowired
     private AlipayProperties alipayProperties;
