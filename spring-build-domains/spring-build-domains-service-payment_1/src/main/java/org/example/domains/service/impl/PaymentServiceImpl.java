b99e7ce7da76713133954408c1c30dadeb018d1f
yuan50697105 <710575900@qq.com>
2021-03-30 10:31:46 +0800

    [新增功能](develop):

---
+++ spring-build-domains/spring-build-domains-service-payment_1/src/main/java/org/example/domains/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,11 @@
+package org.example.domains.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.example.domains.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+}
