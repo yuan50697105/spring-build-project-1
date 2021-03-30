5139b3fcd2ad88a4b05c7c1af622234ffd56b567
yuan50697105 <710575900@qq.com>
2021-03-30 21:15:58 +0800

    [新增功能](develop):

--- spring-build-launchers/spring-build-launchers-nacos-admin_1/src/main/java/org/example/Application.java
+++ spring-build-launchers/spring-build-launchers-nacos-admin_1/src/main/java/org/example/Application.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import cn.hutool.extra.spring.EnableSpringUtil;
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +19,7 @@ import java.util.Arrays;
 @EnableSwagger2WebMvc
 @EnableSpringUtil
 @SpringBootApplication(scanBasePackages = {"org.example"})
-@EnableApolloConfig
+@EnableNacosConfig
 public class Application {
     @Autowired
     private ApplicationContext context;
