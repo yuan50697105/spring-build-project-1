acfa274c0933d7f9dee88352f6d95075af73d167
yuan50697105 <710575900@qq.com>
2021-04-01 12:58:07 +0800

    [新增功能](develop):

--- spring-build-modules/spring-build-modules-repository-mysql-order_1/src/main/java/org/example/modules/repository/mysql/repository/impl/OrderInfoQueryRepositoryImpl.java
+++ spring-build-modules/spring-build-modules-repository-mysql-order_1/src/main/java/org/example/modules/repository/mysql/repository/impl/OrderInfoQueryRepositoryImpl.java
@@ -1,18 +1,30 @@
 package org.example.modules.repository.mysql.repository.impl;
 
+import lombok.AllArgsConstructor;
 import org.example.modules.repository.mysql.builder.OrderInfoBuider;
 import org.example.modules.repository.mysql.dao.TOrderInfoDao;
+import org.example.modules.repository.mysql.entity.query.OrderInfoQuery;
 import org.example.modules.repository.mysql.entity.result.OrderInfo;
 import org.example.modules.repository.mysql.repository.OrderInfoQueryRepository;
+import org.example.plugins.mybatis.entity.IPageData;
+import org.example.plugins.mybatis.repository.impl.IBaseRepositoryImpl;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public class OrderInfoQueryRepositoryImpl extends OrderInfoRepositoryImpl<OrderInfo> implements OrderInfoQueryRepository {
+    private final TOrderInfoDao orderInfoDao;
+    private final OrderInfoBuider orderInfoBuider;
+
     public OrderInfoQueryRepositoryImpl(TOrderInfoDao orderInfoDao, OrderInfoBuider orderInfoBuider) {
         super(orderInfoDao, orderInfoBuider);
+        this.orderInfoDao = orderInfoDao;
+        this.orderInfoBuider = orderInfoBuider;
     }
+
 
     @Override
     public void save(OrderInfo orderInfo) {
