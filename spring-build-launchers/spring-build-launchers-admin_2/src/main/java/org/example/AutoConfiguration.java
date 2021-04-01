a8e1e2100aa6f02d324e500e66036a613a9824e2
yuan50697105 <710575900@qq.com>
2021-04-01 08:59:24 +0800

    [新增功能](develop):

---
+++ spring-build-launchers/spring-build-launchers-admin_2/src/main/java/org/example/AutoConfiguration.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+@EnableCaching
+@EnableSpringUtil
+@ComponentScan
+public class AutoConfiguration {
+}
--- spring-build-launchers/spring-build-launchers-war-admin_1/src/main/java/org/example/Application.java
+++ spring-build-launchers/spring-build-launchers-war-admin_1/src/main/java/org/example/Application.java
@@ -6,17 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @EnableKnife4j
-@EnableSwagger2WebMvc
-@EnableSpringUtil
+@EnableSwagger2
+@EnableOpenApi
 @SpringBootApplication(scanBasePackages = {"org.example"})
 public class Application {
     @Autowired
