3e48e9cfa4f7b583e08fa3a1e80b3f18b96ecfa4
yuan50697105 <710575900@qq.com>
2021-04-11 13:38:12 +0800

    [新增功能](develop):

--- spring-build-infrastructures/spring-build-infrastructures-mysql_1/src/main/java/org/example/spring/infrastructures/mysql/customer/entity/vo/CustomerFormVo.java
+++ spring-build-infrastructures/spring-build-infrastructures-mysql_1/src/main/java/org/example/spring/infrastructures/mysql/customer/entity/vo/CustomerFormVo.java
@@ -1,5 +1,6 @@
 package org.example.spring.infrastructures.mysql.customer.entity.vo;
 
+import cn.hutool.core.util.EnumUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.example.spring.plugins.commons.entity.vo.IBaseVo;
@@ -7,5 +8,38 @@ import org.example.spring.plugins.commons.entity.vo.IBaseVo;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CustomerFormVo extends IBaseVo {
+    private Operating operating;
     private CustomerVo customer;
+    private CustomerContractVo contract;
+
+    public CustomerFormVo withOperating(String name) {
+        setOperating(Operating.get(name));
+        return this;
+    }
+
+    public enum Operating {
+        all("", "0"),
+        customer("customer", "1"),
+        contract("contract", "2");
+
+        public static Operating get(String name) {
+            return EnumUtil.likeValueOf(Operating.class, name);
+        }
+        private String name;
+        private String value;
+
+        Operating(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
 }
--- spring-build-infrastructures/spring-build-infrastructures-mysql_2/src/main/java/org/example/spring/infrastructures/mysql/patient/entity/vo/PatientGroupFormVo.java
+++ spring-build-infrastructures/spring-build-infrastructures-mysql_2/src/main/java/org/example/spring/infrastructures/mysql/patient/entity/vo/PatientGroupFormVo.java
@@ -1,7 +1,10 @@
 package org.example.spring.infrastructures.mysql.patient.entity.vo;
 
+import cn.hutool.core.util.EnumUtil;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.example.spring.plugins.commons.entity.vo.IBaseVo;
 
 import java.util.List;
@@ -9,5 +12,29 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PatientGroupFormVo extends IBaseVo {
+    private Operating operating;
     private PatientGroupVo group;
+    private List<PatientGroupItemVo> items;
+    private List<PatientVo> patients;
+
+    public PatientGroupFormVo withOperating(String name) {
+        this.setOperating(Operating.get(name));
+        return this;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum Operating {
+        ADD("", "0"),
+        ADD_GROUP("group", "1"),
+        ADD_ITEM("item", "2"),
+        ADD_PATIENT("patient", "3"),
+        ;
+        private String name;
+        private String value;
+
+        public static Operating get(String value) {
+            return EnumUtil.likeValueOf(Operating.class, value);
+        }
+    }
 }
