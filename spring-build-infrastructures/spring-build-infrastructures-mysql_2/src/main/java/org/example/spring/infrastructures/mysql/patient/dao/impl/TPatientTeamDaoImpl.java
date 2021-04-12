c0f3adb8974ef1c807751ef085b8a671949b6316
yuan50697105 <710575900@qq.com>
2021-04-12 08:58:25 +0800

    [新增功能](develop):

--- spring-build-infrastructures/spring-build-infrastructures-mysql_2/src/main/java/org/example/spring/infrastructures/mysql/patient/dao/impl/TPatientTeamDaoImpl.java
+++ spring-build-infrastructures/spring-build-infrastructures-mysql_2/src/main/java/org/example/spring/infrastructures/mysql/patient/dao/impl/TPatientTeamDaoImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import lombok.AllArgsConstructor;
 import org.example.spring.infrastructures.mysql.patient.dao.TPatientTeamDao;
 import org.example.spring.infrastructures.mysql.patient.mapper.TPatientGroupMapper;
+import org.example.spring.infrastructures.mysql.patient.mapper.TPatientTeamMapper;
 import org.example.spring.infrastructures.mysql.patient.table.po.TPatientTeam;
 import org.example.spring.infrastructures.mysql.patient.table.query.TPatientTeamQuery;
 import org.example.spring.plugins.mybatis.dao.impl.TkBaseDaoImpl;
@@ -13,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @AllArgsConstructor
 @Transactional
-public class TPatientTeamDaoImpl extends TkBaseDaoImpl<TPatientTeam, TPatientTeamQuery, TPatientGroupMapper> implements TPatientTeamDao {
+public class TPatientTeamDaoImpl extends TkBaseDaoImpl<TPatientTeam, TPatientTeamQuery, TPatientTeamMapper> implements TPatientTeamDao {
     @Override
     protected Wrapper<TPatientTeam> queryWrapper(TPatientTeamQuery tPatientTeamQuery) {
         return null;
