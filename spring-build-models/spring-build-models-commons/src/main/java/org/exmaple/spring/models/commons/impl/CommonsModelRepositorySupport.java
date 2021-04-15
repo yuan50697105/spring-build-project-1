21b27016d6ec9d212bfaceab86fa8da008bc630c
yuan50697105 <710575900@qq.com>
2021-04-15 09:37:31 +0800

    [新增功能](develop):

---
+++ spring-build-models/spring-build-models-commons/src/main/java/org/exmaple/spring/models/commons/impl/CommonsModelRepositorySupport.java
@@ -0,0 +1,6 @@
+package org.exmaple.spring.models.commons.impl;
+
+import org.exmaple.spring.models.commons.CommonsModelRepository;
+
+public abstract class CommonsModelRepositorySupport<T, V, D, Q> implements CommonsModelRepository<T, V, D, Q> {
+}
---
+++ spring-build-models/spring-build-models_1/src/main/java/org/example/spring/models/auth/AccountModelRespoistory.java
@@ -0,0 +1,4 @@
+package org.example.spring.models.auth;
+
+public interface AccountModelRespoistory extends Comm{
+}
