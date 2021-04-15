530c59e0877e93da70e745f1c343c668d8eac68b
yuan50697105 <710575900@qq.com>
2021-04-15 17:05:49 +0800

    [新增功能](develop): 调整模块

--- spring-build-plugins/spring-build-plugins-elasticsearch/src/main/java/org/example/spring/plugins/elasticsearch/entity/query/TkBaseQuery.java
+++
@@ -1,7 +0,0 @@
-package org.example.spring.plugins.elasticsearch.entity.query;
-
-import com.gitee.denger.mybatis.example.ext.MybatisExampleForTk;
-import tk.mybatis.mapper.entity.Example;
-
-public abstract class TkBaseQuery<T> extends EBaseQuery<Example> implements MybatisExampleForTk<T, Example> {
-}
