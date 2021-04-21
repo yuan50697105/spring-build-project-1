47fb466d9de38fd73505cba1fcf086ef24573301
yuan50697105 <710575900@qq.com>
2021-04-21 09:12:05 +0800

    [代码完善](develop): 处理分组体检者处理

--- spring-build-models/spring-build-models-commons/src/main/java/org/example/spring/models/enumerate/UserStatus.java
+++ spring-build-models/spring-build-models-commons/src/main/java/org/example/spring/models/enumerate/UserStatus.java
@@ -10,14 +10,14 @@ import org.example.spring.plugins.commons.converter.IBaseEnum;
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum UserStatus implements IBaseEnum {
-    Normal("正常", "001", 0),
-    Resign("离职", "002", 1),
-    Forbidden("禁用", "003", 2),
+    Normal("正常", "001", "0"),
+    Resign("离职", "002", "1"),
+    Forbidden("禁用", "003", "2"),
     ;
 
     private final String name;
     private final String value;
-    private final Integer code;
+    private final String code;
 
     public static UserStatus get(String value) {
         return EnumUtil.likeValueOf(UserStatus.class, value);
