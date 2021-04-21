0f91fff2933e7bded3a27cbdea2be7185e76aab9
yuan50697105 <710575900@qq.com>
2021-04-21 09:04:07 +0800

    [代码完善](develop): 处理分组体检者处理

--- spring-build-models/spring-build-models_2/src/main/java/org/example/spring/models/auth/entity/vo/AccountVo.java
+++ spring-build-models/spring-build-models_2/src/main/java/org/example/spring/models/auth/entity/vo/AccountVo.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import org.example.spring.models.enumerate.UserStatus;
 import org.example.spring.infrastructures.mysql.auth.table.po.TUser;
 
+import java.util.Optional;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AccountVo extends TUser {
@@ -12,6 +14,6 @@ public class AccountVo extends TUser {
 
     @Override
     public void setStatus(String status) {
-        super.setStatus(UserStatus.get(status).getValue());
+        super.setStatus(Optional.ofNullable(UserStatus.get(status)).map(UserStatus::getValue).orElse(null));
     }
 }
