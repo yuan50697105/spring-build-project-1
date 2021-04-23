40259a624d9d421a05a59e4fc3a29974ba105c9c
yuan50697105 <710575900@qq.com>
2021-04-23 08:25:35 +0800

    [代码完善](develop): 体检者套餐补全体检者ID

--- spring-build-infrastructures/spring-build-infrastructures-clickhouse_2/src/main/java/org/example/spring/infrastructures/clickhouse/ClickHouseConfiguration.java
+++ spring-build-infrastructures/spring-build-infrastructures-clickhouse_2/src/main/java/org/example/spring/infrastructures/clickhouse/ClickHouseConfiguration.java
@@ -1,4 +1,4 @@
-package org.example.spring.infrastructures.postgresql;
+package org.example.spring.infrastructures.clickhouse;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
