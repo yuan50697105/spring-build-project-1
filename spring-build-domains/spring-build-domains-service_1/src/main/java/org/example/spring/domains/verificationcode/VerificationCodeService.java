bc2cab9da0a875d32ea373a0d4e39ea9bbcacab2
yuan50697105 <710575900@qq.com>
2021-04-24 21:17:13 +0800

    [新增功能](develop): meal

--- spring-build-domains/spring-build-domains-service_1/src/main/java/org/example/spring/domains/verificationcode/VerificationCodeService.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.domains.verificationcode;
-
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-import com.aliyuncs.exceptions.ClientException;
-
-public interface VerificationCodeService {
-    SendSmsResponse send() throws ClientException;
-}
