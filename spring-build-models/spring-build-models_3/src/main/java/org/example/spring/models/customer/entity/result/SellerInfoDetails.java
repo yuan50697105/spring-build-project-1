ddc37694d62fded7ee54dab3f2a3abe5c8ce3ec7
yuan50697105 <710575900@qq.com>
2021-04-26 14:38:18 +0800

    [代码完善](develop): 处理分组体检者处理

--- spring-build-models/spring-build-models_3/src/main/java/org/example/spring/models/customer/entity/result/SellerInfoDetails.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.models.customer.entity.result;
-
-import lombok.Data;
-
-@Data
-public class SellerInfoDetails {
-    private SellerInfo seller;
-}
--- spring-build-models/spring-build-models_3/src/main/java/org/example/spring/models/patient/entity/result/PatientDetails.java
+++
@@ -1,9 +0,0 @@
-package org.example.spring.models.patient.entity.result;
-
-import lombok.Data;
-
-@Data
-public class PatientDetails {
-    private Patient patient;
-
-}
