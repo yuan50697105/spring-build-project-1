26dbc3ebf9ab26b40b62040a882e009dcef51ac9
yuan50697105 <710575900@qq.com>
2021-04-26 14:01:15 +0800

    [新增功能](develop): mysql 微服务组件

--- spring-build-infrastructures-microservices/spring-build-infrastructures-microservices-mysql-modules/spring-build-infrastructures-microservices-mysql_2/src/main/java/org/example/spring/infrastructures/mysql/patient/dao/TPatientTeamDao.java
+++
@@ -1,9 +0,0 @@
-package org.example.spring.infrastructures.mysql.patient.dao;
-
-import org.example.spring.infrastructures.mysql.patient.table.po.TPatientTeam;
-import org.example.spring.infrastructures.mysql.patient.table.query.TPatientTeamQuery;
-import org.example.spring.plugins.mybatis.dao.TkBaseDao;
-
-public interface TPatientTeamDao extends TkBaseDao<TPatientTeam, TPatientTeamQuery> {
-    boolean existById(Long id);
-}
