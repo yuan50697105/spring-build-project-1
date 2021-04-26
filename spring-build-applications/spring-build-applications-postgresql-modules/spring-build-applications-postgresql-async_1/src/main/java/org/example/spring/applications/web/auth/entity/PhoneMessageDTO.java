566fb76b96708de177f880777aa40d4b8362c26b
yuan50697105 <710575900@qq.com>
2021-04-26 16:30:01 +0800

    [新增功能](develop): RxController

---
+++ spring-build-applications/spring-build-applications-postgresql-modules/spring-build-applications-postgresql-async_1/src/main/java/org/example/spring/applications/web/auth/entity/PhoneMessageDTO.java
@@ -0,0 +1,9 @@
+package org.example.spring.applications.web.auth.entity;
+
+import lombok.Data;
+
+@Data
+public class PhoneMessageDTO {
+    private String phone;
+
+}
---
+++ spring-build-applications/spring-build-applications-postgresql-modules/spring-build-applications-postgresql_2/src/main/java/org/example/spring/applications/web/auth/controller/ResourceController.java
@@ -0,0 +1,69 @@
+package org.example.spring.applications.web.auth.controller;
+
+import ai.yue.library.base.view.R;
+import ai.yue.library.base.view.Result;
+import cn.hutool.core.lang.tree.Tree;
+import lombok.AllArgsConstructor;
+import org.example.spring.domains.postgresql.auth.service.ResourceService;
+import org.example.spring.models.postgresql.auth.entity.query.ResourceQuery;
+import org.example.spring.models.postgresql.auth.entity.result.ResourceDetails;
+import org.example.spring.models.postgresql.auth.entity.vo.ResourceModelVo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("resource")
+@AllArgsConstructor
+public class ResourceController {
+    private final ResourceService resourceService;
+
+    @GetMapping("list/{id}")
+    public Result<List<Tree<Long>>> listByUserId(@PathVariable Long id) {
+        List<Tree<Long>> data = resourceService.listResourceByUserId(id);
+        return R.success(data);
+    }
+
+    @GetMapping
+    public Result<List<Tree<Long>>> data(ResourceQuery query) {
+        return R.success(resourceService.selectTreeList(query));
+    }
+
+    @GetMapping("{id}")
+    public Result<ResourceDetails> get(@PathVariable Long id) {
+        ResourceDetails details = resourceService.get(id);
+        return R.success(details);
+    }
+
+    @PostMapping
+    public Result<?> save(@RequestBody ResourceModelVo formVo) {
+        resourceService.save(formVo);
+        return R.success();
+    }
+
+    @PutMapping
+    public Result<?> update(@RequestBody ResourceModelVo formVo) {
+        resourceService.update(formVo);
+        return R.success();
+    }
+
+
+    @PutMapping("{id}")
+    public Result<?> update(@RequestBody ResourceModelVo formVo, @PathVariable Long id) {
+        resourceService.update((ResourceModelVo) formVo.withId(id));
+        return R.success();
+    }
+
+    @DeleteMapping("{id}")
+    public Result<?> delete(@PathVariable Long id) {
+        resourceService.delete(Collections.singletonList(id));
+        return R.success();
+    }
+
+    @DeleteMapping
+    public Result<?> delete(List<Long> ids) {
+        resourceService.delete(ids);
+        return R.success();
+    }
+}
