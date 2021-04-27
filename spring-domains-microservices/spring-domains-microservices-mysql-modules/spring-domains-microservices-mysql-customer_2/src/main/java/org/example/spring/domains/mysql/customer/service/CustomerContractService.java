27e184f85fd1d04bcaddf2a99713e06b8204e5b4
yuan50697105 <710575900@qq.com>
2021-04-27 21:05:53 +0800

    aaa

---
+++ spring-domains-microservices/spring-domains-microservices-mysql-modules/spring-domains-microservices-mysql-customer_2/src/main/java/org/example/spring/domains/mysql/customer/service/CustomerContractService.java
@@ -0,0 +1,37 @@
+package org.example.spring.domains.mysql.customer.service;
+
+import org.example.spring.models.mysql.customer.entity.query.CustomerContractQuery;
+import org.example.spring.models.mysql.customer.entity.result.CustomerContract;
+import org.example.spring.models.mysql.customer.entity.result.CustomerContractDetails;
+import org.example.spring.models.mysql.customer.entity.vo.CustomerContractModelVo;
+import org.example.spring.plugins.commons.entity.IPageData;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerContractService {
+    IPageData<CustomerContract> selectPage(CustomerContractQuery query);
+
+    List<CustomerContract> selectList(CustomerContractQuery query);
+
+    CustomerContract selectOne(CustomerContractQuery query);
+
+    default Optional<CustomerContract> seletOneOpt(CustomerContractQuery query) {
+        return Optional.ofNullable(selectOne(query));
+    }
+
+    CustomerContractDetails get(Long id);
+
+    default Optional<CustomerContractDetails> getOpt(Long id) {
+        return Optional.ofNullable(get(id));
+    }
+
+    void save(CustomerContractModelVo formVo);
+
+    void update(CustomerContractModelVo formVo);
+
+    void delete(List<Long> ids);
+
+    void upload(Long id, MultipartFile file);
+}
