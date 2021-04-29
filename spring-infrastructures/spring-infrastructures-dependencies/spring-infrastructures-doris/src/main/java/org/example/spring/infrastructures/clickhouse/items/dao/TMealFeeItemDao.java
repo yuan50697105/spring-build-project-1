6771c2ecaa6abefe7adecdd307f829b0cba35307
yuan50697105 <710575900@qq.com>
2021-04-29 08:36:44 +0800

    [新增功能](develop): test模块

--- spring-infrastructures/spring-infrastructures-dependencies/spring-infrastructures-doris/src/main/java/org/example/spring/infrastructures/clickhouse/items/dao/TMealFeeItemDao.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.infrastructures.clickhouse.items.dao;
-
-import org.example.spring.infrastructures.clickhouse.items.table.po.TMealFeeItem;
-import org.example.spring.infrastructures.clickhouse.items.table.query.TMealFeeItemQuery;
-import org.example.spring.plugins.mybatis.dao.TkBaseDao;
-
-public interface TMealFeeItemDao extends TkBaseDao<TMealFeeItem, TMealFeeItemQuery> {
-}
--- spring-infrastructures/spring-infrastructures-dependencies/spring-infrastructures-doris/src/main/java/org/example/spring/infrastructures/clickhouse/patient/dao/TPatientCheckItemDao.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.infrastructures.clickhouse.patient.dao;
-
-import org.example.spring.infrastructures.clickhouse.patient.table.po.TPatientCheckItem;
-import org.example.spring.infrastructures.clickhouse.patient.table.query.TPatientCheckItemQuery;
-import org.example.spring.plugins.mybatis.dao.TkBaseDao;
-
-public interface TPatientCheckItemDao extends TkBaseDao<TPatientCheckItem, TPatientCheckItemQuery> {
-}
