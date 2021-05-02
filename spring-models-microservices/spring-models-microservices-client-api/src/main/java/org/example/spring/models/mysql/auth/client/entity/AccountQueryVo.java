d4c172d9484dac0dd39d524e9e30ee2019a539b2
yuan50697105 <710575900@qq.com>
2021-05-02 23:53:49 +0800

    [新增功能](develop): test模块

--- spring-models-microservices/spring-models-microservices-client-api/src/main/java/org/example/spring/models/mysql/auth/client/entity/AccountQueryVo.java
+++ spring-models-microservices/spring-models-microservices-client-api/src/main/java/org/example/spring/models/mysql/auth/client/entity/AccountQueryVo.java
@@ -5,10 +5,16 @@ import lombok.EqualsAndHashCode;
 import org.example.spring.models.commons.enumerate.UserStatus;
 import org.example.spring.plugins.commons.entity.query.BaseQuery;
 
+import java.util.Optional;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AccountQueryVo extends BaseQuery {
     private String username;
     private String name;
-    private UserStatus status;
+    private String status;
+
+    public void setStatus(String status) {
+        this.status = Optional.ofNullable(UserStatus.get(status)).map(UserStatus::getValue).orElse(null);
+    }
 }
