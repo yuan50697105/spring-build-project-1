9eed0ca0f68cad19cbed410e827b481e3b277b4f
yuan50697105 <710575900@qq.com>
2021-05-03 15:04:49 +0800

    修改发行脚本

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 echo off
 call mvn clean release:prepare release:perform -B -Pd
 call mvn clean deploy -B -Pd
-call git push --all
-call git push --tag
+call git push --all -u
+call git push --tag -u
