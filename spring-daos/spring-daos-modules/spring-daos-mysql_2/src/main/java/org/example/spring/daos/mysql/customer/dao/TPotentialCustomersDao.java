89cf18ac60f681999743b1464b970ad2c1fa30e2
yuan50697105 <710575900@qq.com>
2021-05-04 08:06:20 +0800

    [新增功能](develop): 客户订单 [ci-build]
    Signed-off-by: yuan50697105 <710575900@qq.com>

--- spring-daos/spring-daos-modules/spring-daos-mysql_2/src/main/java/org/example/spring/daos/mysql/customer/dao/TPotentialCustomersDao.java
+++ spring-daos/spring-daos-modules/spring-daos-mysql_2/src/main/java/org/example/spring/daos/mysql/customer/dao/TPotentialCustomersDao.java
@@ -1,2 +1,8 @@
-package org.example.spring.daos.mysql.customer.dao;public interface TPotentialCustomersDao {
+package org.example.spring.daos.mysql.customer.dao;
+
+import org.example.spring.daos.mysql.customer.table.po.TPotentialCustomers;
+import org.example.spring.daos.mysql.customer.table.query.TPotentialCustomersQuery;
+import org.example.spring.plugins.mybatis.dao.TkBaseDao;
+
+public interface TPotentialCustomersDao extends TkBaseDao<TPotentialCustomers, TPotentialCustomersQuery> {
 }
