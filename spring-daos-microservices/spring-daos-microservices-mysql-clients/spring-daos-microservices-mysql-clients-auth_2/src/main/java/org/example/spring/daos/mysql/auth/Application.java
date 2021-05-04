79ab4137c5fb3a5b3ec9258daaf1477bef9a4673
yuan50697105 <710575900@qq.com>
2021-05-04 11:19:15 +0800

    [Jenkins](develop): [ci-build]

--- spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
+++ spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
@@ -1,11 +1,7 @@
 package org.example.spring.daos.mysql.auth;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 }
---
+++ spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/client/impl/ResourceDaoClientImpl.java
@@ -0,0 +1,80 @@
+package org.example.spring.daos.mysql.auth.client.impl;
+
+import lombok.AllArgsConstructor;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.example.spring.daos.mysql.auth.builder.AuthClientBuilder;
+import org.example.spring.daos.mysql.auth.client.ResourceDaoClient;
+import org.example.spring.daos.mysql.auth.dao.TResourceDao;
+import org.example.spring.daos.mysql.auth.entity.Resource;
+import org.example.spring.daos.mysql.auth.entity.query.ResourceQuery;
+import org.example.spring.daos.mysql.auth.table.po.TResource;
+import org.example.spring.daos.mysql.auth.table.query.TResourceQuery;
+import org.example.spring.plugins.commons.entity.IPageData;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ValidationException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@DubboService
+@AllArgsConstructor
+@Transactional
+public class ResourceDaoClientImpl implements ResourceDaoClient {
+    private final AuthClientBuilder authClientBuilder;
+    private final TResourceDao resourceDao;
+
+    @Override
+    public void save(Resource resource) {
+        resourceDao.save(authClientBuilder.createForSave(resource));
+    }
+
+    @Override
+    public void update(Resource resource) {
+        Optional<TResource> optional = resourceDao.getByIdOpt(resource.getId());
+        if (optional.isPresent()) {
+            TResource tResource = optional.get();
+            tResource.copy(authClientBuilder.createForSave(resource));
+            resourceDao.updateById(tResource);
+        }
+    }
+
+    @Override
+    public void delete(List<Long> ids) {
+        validateDelete(ids);
+    }
+
+    private void validateDelete(List<Long> ids) {
+        if (resourceDao.validateDelete(ids)) {
+            throw new ValidationException("存在下级菜单");
+        }
+    }
+
+    @Override
+    public Resource get(Long id) {
+        return authClientBuilder.createForGetResource(resourceDao.getById(id));
+    }
+
+    @Override
+    public Optional<Resource> get(ResourceQuery query) {
+        TResourceQuery resourceQuery = authClientBuilder.createForQuery(query);
+        Optional<TResource> optional = resourceDao.queryFirst(resourceQuery);
+        return Optional.ofNullable(authClientBuilder.createForGetResource(optional.orElse(null)));
+    }
+
+    @Override
+    public List<Resource> list(ResourceQuery query) {
+        TResourceQuery resourceQuery = authClientBuilder.createForQuery(query);
+        List<TResource> resourceList = resourceDao.queryTop(resourceQuery, resourceQuery.getSize());
+        return authClientBuilder.createForGetResource(resourceList);
+    }
+
+    @Override
+    public IPageData<Resource> data(ResourceQuery query) {
+        TResourceQuery resourceQuery = authClientBuilder.createForQuery(query);
+        IPageData<TResource> pageData = resourceDao.queryPage(resourceQuery);
+        return authClientBuilder.createForGetResource(pageData);
+    }
+
+}
