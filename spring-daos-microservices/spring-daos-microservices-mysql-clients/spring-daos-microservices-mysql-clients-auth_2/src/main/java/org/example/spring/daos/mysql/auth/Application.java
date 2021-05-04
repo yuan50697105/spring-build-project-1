e661fc5b3ec564a296d27290374cfb4081806de2
yuan50697105 <710575900@qq.com>
2021-05-04 14:17:19 +0800

    [Jenkins](develop): [ci-build]

--- spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
+++ spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
@@ -1,7 +1,13 @@
 package org.example.spring.daos.mysql.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+@SpringBootApplication
 @SpringCloudApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
+++ spring-daos-microservices/spring-daos-microservices-mysql-clients/spring-daos-microservices-mysql-clients_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
@@ -1,8 +1,10 @@
 package org.example.spring.daos.mysql.auth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+@SpringBootApplication
 @SpringCloudApplication
 public class Application {
     public static void main(String[] args) {
