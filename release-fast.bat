805518cb734d7f933bbcd6de9a26582b82b9ba20
yuan50697105 <710575900@qq.com>
2021-05-06 09:51:28 +0800

    aa

--- release-fast.bat
+++ release-fast.bat
@@ -1,5 +1,5 @@
 echo off
-call mvn clean release:prepare release:perform -B -Pd -T 1C
-call mvn clean deploy -B -Pd -T 1C
+call mvn clean release:prepare release:perform -B -Pd -T 1C -fn
+call mvn clean deploy -B -Pd -T 1C -fn
 call git push --all
 call git push --tag
--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 echo off
-call mvn clean release:prepare release:perform -B -Pd
-call mvn clean deploy -B -Pd
+call mvn clean release:prepare release:perform -B -Pd -fn
+call mvn clean deploy -B -Pd -fn
 call git push --all -u
 call git push --tag -u
