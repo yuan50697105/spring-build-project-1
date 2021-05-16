159f4e853f5da56ab665daf8ce3de385507351ca
yuan50697105 <710575900@qq.com>
2021-05-17 07:17:42 +0800

    a

--- spring-micro-daos/spring-micro-daos-mysql-rws-clients/spring-micro-daos-mysql-rws-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
+++ spring-micro-daos/spring-micro-daos-mysql-rws-clients/spring-micro-daos-mysql-rws-clients-auth_2/src/main/java/org/example/spring/daos/mysql/auth/Application.java
@@ -1,15 +1,15 @@
 package org.example.spring.daos.mysql.auth;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @SpringCloudApplication
-@EnableDubboConfig
 @EnableDubbo
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
