c57a59e8a1609cd2a59b6570ddc1a17ba245972e
yuan50697105 <710575900@qq.com>
2021-05-19 09:55:02 +0800

    [功能修改]:mybatis 功能增强

--- spring-plugins/spring-plugins-modules/spring-plugins-mybatis_2/src/main/java/org/example/spring/plugins/mybatis/converter/IBaseConverter.java
+++ spring-plugins/spring-plugins-modules/spring-plugins-mybatis_2/src/main/java/org/example/spring/plugins/mybatis/converter/IBaseConverter.java
@@ -2,13 +2,14 @@ package org.example.spring.plugins.mybatis.converter;
 
 import org.example.spring.plugins.commons.entity.IPageData;
 import org.example.spring.plugins.mybatis.entity.query.IBaseQuery;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
 public interface IBaseConverter<T, DTO extends T, VO extends T, Q extends IBaseQuery> {
     T buildPo(VO vo);
 
-    void copy(VO vo, T t);
+    void copy(VO vo, @MappingTarget T t);
 
     DTO buildDTO(T byId);
 
