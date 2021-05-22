b2e7b4938879f43bece3e3779d16df3584fcdf4f
yuan50697105 <710575900@qq.com>
2021-05-22 10:17:04 +0800

    aaaaa

--- spring-applications/spring-applications-postgresql-modules/spring-applications-postgresql_2/src/main/java/org/example/spring/applications/web/PostgresqlApplicationAutoConfiguration.java
+++ spring-applications/spring-applications-postgresql-modules/spring-applications-postgresql_2/src/main/java/org/example/spring/applications/web/PostgresqlApplicationAutoConfiguration.java
@@ -3,5 +3,5 @@ package org.example.spring.applications.web;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AdminAutoConfiguration {
+public class PostgresqlApplicationAutoConfiguration {
 }
