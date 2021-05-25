7e38b5d69c259cbe509bdba62c4684e15bf217fa
yuan50697105 <710575900@qq.com>
2021-05-25 22:55:00 +0800

    a

--- spring-domains-micro-repositories/spring-domains-micro-repositories-client-api/src/main/java/org/example/spring/models/mysql/auth/client/IDAccountClient.java
+++
@@ -1,7 +0,0 @@
-package org.example.spring.models.mysql.auth.client;
-
-import org.example.spring.daos.mysql.auth.client.IUserRepositoryClient;
-
-public interface IDAccountClient extends IUserRepositoryClient {
-
-}
--- spring-micro-daos-clients/spring-micro-daos-api-clients/spring-micro-daos-api-clients-api/src/main/java/org/example/spring/daos/mysql/patient/entity/PatientDetails.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.daos.mysql.patient.entity;
-
-import lombok.Data;
-
-@Data
-public class PatientDetails {
-    private Patient patient;
-}
