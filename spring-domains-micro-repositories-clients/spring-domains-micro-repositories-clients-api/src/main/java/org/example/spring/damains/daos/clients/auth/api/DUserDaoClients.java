f9c118161312b14c04036189d90c02d5bf3ff3b4
yuan50697105 <710575900@qq.com>
2021-05-28 11:06:52 +0800

    a

---
+++ spring-domains-micro-repositories-clients/spring-domains-micro-repositories-clients-api/src/main/java/org/example/spring/damains/daos/clients/auth/api/DUserDaoClients.java
@@ -0,0 +1,6 @@
+package org.example.spring.damains.daos.clients.auth.api;
+
+import org.example.spring.daos.clients.auth.api.UserDaoClients;
+
+public interface DUserDaoClients extends UserDaoClients {
+}
