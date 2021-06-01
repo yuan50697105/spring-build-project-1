5ee322568143a526d9492086520a7403b3140bcc
yuan50697105 <710575900@qq.com>
2021-06-01 17:24:12 +0800

    aa

--- spring-micro-repositories/spring-micro-repositories-mssql/spring-micro-repositories-mssql-modules_2/spring-micro-repositories-mssql-auth_2/src/main/java/org/example/spring/daos/postgres/auth/dao/TUserResourceDao.java
+++
@@ -1,9 +0,0 @@
-package org.example.spring.daos.postgres.auth.dao;
-
-import org.example.spring.daos.postgres.auth.table.po.TResource;
-
-import java.util.List;
-
-public interface TUserResourceDao {
-    List<TResource> listResourceByUserId(Long userId);
-}
---
+++ spring-micro-repositories/spring-micro-repositories-mssql/spring-micro-repositories-mssql-modules_2/spring-micro-repositories-mssql-auth_2/src/main/java/org/example/spring/repositories/postgres/auth/table/po/TUser.java
@@ -0,0 +1,57 @@
+package org.example.spring.repositories.postgres.auth.table.po;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.example.spring.daos.mysql.table.enumerate.TUserStatus;
+import org.example.spring.plugins.mybatis.entity.po.IBaseEntity;
+
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * t_user
+ *
+ * @author yuane
+ */
+@ApiModel(value = "org-example-spring-daos-mysql-org.example.spring.daos.mysql.auth-table-po-TUser")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(TUser.TABLE)
+@Table(name = TUser.TABLE)
+public class TUser extends IBaseEntity implements Serializable {
+
+    public static final String TABLE = "t_user";
+    private static final long serialVersionUID = 2799305908245541459L;
+    /**
+     * 用户名
+     */
+    @ApiModelProperty(value = "用户名")
+    private String username;
+
+    /**
+     * 姓名
+     */
+    @ApiModelProperty(value = "姓名")
+    private String name;
+
+    /**
+     * 密码
+     */
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    /**
+     * 状态
+     *
+     * @see TUserStatus
+     */
+    @ApiModelProperty(value = "状态")
+    private String status;
+}
