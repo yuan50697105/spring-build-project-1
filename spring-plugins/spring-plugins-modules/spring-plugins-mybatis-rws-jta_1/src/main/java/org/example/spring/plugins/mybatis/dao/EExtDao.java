05e5aca634f9129afaee5a9a34f2483a672e444d
yuan50697105 <710575900@qq.com>
2021-06-02 08:32:25 +0800

    [代码完善](develop): 修改部分数据库访问处理接口

--- spring-plugins/spring-plugins-modules/spring-plugins-mybatis-rws-jta_1/src/main/java/org/example/spring/plugins/mybatis/dao/EExtDao.java
+++
@@ -1,6 +0,0 @@
-package org.example.spring.plugins.mybatis.dao;
-
-import org.example.spring.plugins.mybatis.entity.query.EExtQuery;
-
-public interface EExtDao<T, Q extends EExtQuery<E>, E> {
-}
--- spring-plugins/spring-plugins-modules/spring-plugins-mybatis-rws-jta_2/src/main/java/org/example/spring/plugins/mybatis/dao/TkExtDao.java
+++
@@ -1,6 +0,0 @@
-package org.example.spring.plugins.mybatis.dao;
-
-import org.example.spring.plugins.mybatis.entity.query.TkExtQuery;
-
-public interface TkExtDao<T, Q extends TkExtQuery<T>> extends TkBaseDao<T, Q> {
-}
