0f2646b4c37e96a74b784a1488b04bd6330e67f6
yuan50697105 <710575900@qq.com>
2021-06-04 13:36:54 +0800

    [代码完善](develop): 完善功能类型

--- spring-micro-repositories/spring-micro-repositories-mssql/spring-micro-repositories-mssql-modules_1/spring-micro-repositories-mssql-bill_1/src/main/java/org/example/spring/repositories/mssql/bill/repository/TBillInfoRepository.java
+++ spring-micro-repositories/spring-micro-repositories-mssql/spring-micro-repositories-mssql-modules_1/spring-micro-repositories-mssql-bill_1/src/main/java/org/example/spring/repositories/mssql/bill/repository/TBillInfoRepository.java
@@ -1,66 +1,10 @@
 package org.example.spring.repositories.mssql.bill.repository;
 
-import org.example.spring.plugins.commons.entity.IPageData;
+import org.example.spring.plugins.mybatis.repository.IBaseRepository;
 import org.example.spring.repositories.mssql.bill.table.dto.TBillInfoDTO;
-import org.example.spring.repositories.mssql.bill.table.dto.TBillInfoDetailsDTO;
+import org.example.spring.repositories.mssql.bill.table.po.TBillInfo;
 import org.example.spring.repositories.mssql.bill.table.query.TBillInfoQuery;
 import org.example.spring.repositories.mssql.bill.table.vo.TBillInfoVo;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-public interface TBillInfoRepository {
-    void save(TBillInfoVo vo);
-
-    void update(TBillInfoVo vo);
-
-    void delete(Long id);
-
-    void delete(Long... ids);
-
-    void delete(List<Long> ids);
-
-    TBillInfoDTO get(Long id);
-
-    Optional<TBillInfoDTO> getOpt(Long id);
-
-    TBillInfoDTO queryOne(TBillInfoQuery query);
-
-    TBillInfoDTO selectOne(TBillInfoQuery query);
-
-    Optional<TBillInfoDTO> selectOneOpt(TBillInfoQuery query);
-
-    Optional<TBillInfoDTO> queryOneOpt(TBillInfoQuery query);
-
-    TBillInfoDTO queryFirst(TBillInfoQuery query);
-
-    Optional<TBillInfoDTO> queryFirstOpt(TBillInfoQuery query);
-
-    TBillInfoDTO selectFirst(TBillInfoQuery query);
-
-    Optional<TBillInfoDTO> selectFirstOpt(TBillInfoQuery query);
-
-    List<TBillInfoDTO> queryList(TBillInfoQuery query);
-
-    List<TBillInfoDTO> selectList(TBillInfoQuery query);
-
-    Stream<TBillInfoDTO> queryStream(TBillInfoQuery query);
-
-    Stream<TBillInfoDTO> selectStream(TBillInfoQuery query);
-
-    List<TBillInfoDTO> queryTop(TBillInfoQuery query);
-
-    Stream<TBillInfoDTO> queryTopStream(TBillInfoQuery query);
-
-    List<TBillInfoDTO> selectTop(TBillInfoQuery query);
-
-    Stream<TBillInfoDTO> selectTopStream(TBillInfoQuery query);
-
-    IPageData<TBillInfoDTO> selectPage(TBillInfoQuery query);
-
-    IPageData<TBillInfoDTO> queryPage(TBillInfoQuery query);
-
-    TBillInfoDetailsDTO getDetails(Long id);
-
+public interface TBillInfoRepository extends IBaseRepository<TBillInfo, TBillInfoDTO, TBillInfoVo, TBillInfoQuery> {
 }
--- spring-micro-repositories/spring-micro-repositories-oracle/spring-micro-repositories-oracle-modules_1/spring-micro-repositories-oracle-bill_1/src/main/java/org/example/spring/repositories/oracle/bill/repository/TBillLogRepository.java
+++ spring-micro-repositories/spring-micro-repositories-oracle/spring-micro-repositories-oracle-modules_1/spring-micro-repositories-oracle-bill_1/src/main/java/org/example/spring/repositories/oracle/bill/repository/TBillLogRepository.java
@@ -1,63 +1,10 @@
 package org.example.spring.repositories.oracle.bill.repository;
 
-import org.example.spring.plugins.commons.entity.IPageData;
+import org.example.spring.plugins.mybatis.repository.IBaseRepository;
 import org.example.spring.repositories.oracle.bill.table.dto.TBillLogDTO;
+import org.example.spring.repositories.oracle.bill.table.po.TBillLog;
 import org.example.spring.repositories.oracle.bill.table.query.TBillLogQuery;
 import org.example.spring.repositories.oracle.bill.table.vo.TBillLogVo;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-public interface TBillLogRepository {
-    void save(TBillLogVo vo);
-
-    void update(TBillLogVo vo);
-
-    void delete(Long id);
-
-    void delete(Long... ids);
-
-    void delete(List<Long> ids);
-
-    TBillLogDTO get(Long id);
-
-    Optional<TBillLogDTO> getOpt(Long id);
-
-    TBillLogDTO queryOne(TBillLogQuery query);
-
-    TBillLogDTO selectOne(TBillLogQuery query);
-
-    Optional<TBillLogDTO> selectOneOpt(TBillLogQuery query);
-
-    Optional<TBillLogDTO> queryOneOpt(TBillLogQuery query);
-
-    TBillLogDTO queryFirst(TBillLogQuery query);
-
-    Optional<TBillLogDTO> queryFirstOpt(TBillLogQuery query);
-
-    TBillLogDTO selectFirst(TBillLogQuery query);
-
-    Optional<TBillLogDTO> selectFirstOpt(TBillLogQuery query);
-
-    List<TBillLogDTO> queryList(TBillLogQuery query);
-
-    List<TBillLogDTO> selectList(TBillLogQuery query);
-
-    Stream<TBillLogDTO> queryStream(TBillLogQuery query);
-
-    Stream<TBillLogDTO> selectStream(TBillLogQuery query);
-
-    List<TBillLogDTO> queryTop(TBillLogQuery query);
-
-    Stream<TBillLogDTO> queryTopStream(TBillLogQuery query);
-
-    List<TBillLogDTO> selectTop(TBillLogQuery query);
-
-    Stream<TBillLogDTO> selectTopStream(TBillLogQuery query);
-
-    IPageData<TBillLogDTO> selectPage(TBillLogQuery query);
-
-    IPageData<TBillLogDTO> queryPage(TBillLogQuery query);
-
+public interface TBillLogRepository extends IBaseRepository<TBillLog, TBillLogDTO, TBillLogVo, TBillLogQuery> {
 }
