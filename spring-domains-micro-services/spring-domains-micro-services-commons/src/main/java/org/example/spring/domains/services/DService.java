f44342e2bf73334311666cce9b9a77df0531592a
yuan50697105 <710575900@qq.com>
2021-06-04 11:47:28 +0800

    [新增功能](develop): kk

---
+++ spring-domains-micro-services/spring-domains-micro-services-commons/src/main/java/org/example/spring/domains/services/DService.java
@@ -0,0 +1,6 @@
+package org.example.spring.domains.services;
+
+import org.example.spring.repositories.clients.commons.api.CommonsRepository;
+
+public interface DService<T, V, D, Q> extends CommonsRepository<T, V, D, Q> {
+}
