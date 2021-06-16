73f9f140766e4be0edfd9f4a000dcd1449b51494
yuan50697105 <710575900@qq.com>
2021-06-16 08:40:33 +0800

    [代码重构](develop): oracle auth feign 重构

--- spring-micro-repositories-clients/spring-micro-repositories-clients-oracle/spring-micro-repositories-clients-oracle-feign-modules/spring-micro-repositories-clients-oracle-auth-feign_2/src/main/java/org/example/spring/repositories/clients/auth/oracle/feign/RoleRepositoryController.java
+++ spring-micro-repositories-clients/spring-micro-repositories-clients-oracle/spring-micro-repositories-clients-oracle-feign-modules/spring-micro-repositories-clients-oracle-auth-feign_2/src/main/java/org/example/spring/repositories/clients/auth/oracle/feign/RoleRepositoryController.java
@@ -1,15 +1,19 @@
-package org.example.spring.repositories.clients.auth.mysql.feign;
+package org.example.spring.repositories.clients.auth.oracle.feign;
 
 
 import org.example.spring.repositories.clients.auth.api.RoleRepository;
+import org.example.spring.repositories.commons.entity.auth.dto.RoleDTO;
+import org.example.spring.repositories.commons.entity.auth.po.Role;
+import org.example.spring.repositories.commons.entity.auth.query.RoleQuery;
+import org.example.spring.repositories.commons.entity.auth.vo.RoleVo;
 import org.example.spring.repositories.feign.auth.clients.RoleRepositoryClient;
-import org.example.spring.repositories.feign.auth.clients.impl.RoleRepositoryClientImpl;
+import org.example.spring.repositories.feign.commons.clients.impl.CommonsFeignClientImpl;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Primary
-public class RoleRepositoryController extends RoleRepositoryClientImpl implements RoleRepositoryClient {
+public class RoleRepositoryController extends CommonsFeignClientImpl<Role, RoleVo, RoleDTO, RoleQuery,RoleRepository> implements RoleRepositoryClient {
     public RoleRepositoryController(RoleRepository repository) {
         super(repository);
     }
