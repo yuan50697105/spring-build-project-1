f5f8bcda9b22cafc98ba57c89c722fdb1bf0a58e
yuan50697105 <710575900@qq.com>
2021-06-17 16:38:53 +0800

    [代码完善](develop): .

---
+++ spring-micro-repositories-clients/spring-micro-repositories-clients-mysql/spring-micro-repositories-clients-mysql-dubbo-modules/spring-micro-repositories-clients-mysql-patient-dubbo_2/src/main/java/org/example/spring/repositories/clients/patient/mysql/dubbo/PatientTeamMealFeeItemDubboRepositoryImpl.java
@@ -0,0 +1,17 @@
+package org.example.spring.repositories.clients.patient.mysql.dubbo;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.example.spring.repositories.clients.dubbo.commons.CommonsDubboRepositoryImpl;
+import org.example.spring.repositories.clients.dubbo.patient.api.PatientTeamMealCheckItemDubboRepository;
+import org.example.spring.repositories.clients.patient.api.PatientTeamMealCheckItemRepository;
+import org.example.spring.repositories.commons.entity.patient.dto.PatientTeamMealCheckItemDTO;
+import org.example.spring.repositories.commons.entity.patient.po.PatientTeamMealCheckItem;
+import org.example.spring.repositories.commons.entity.patient.po.PatientTeamMealFeeItem;
+import org.example.spring.repositories.commons.entity.patient.query.PatientTeamMealCheckItemQuery;
+import org.example.spring.repositories.commons.entity.patient.vo.PatientTeamMealCheckItemVo;
+import org.springframework.stereotype.Component;
+
+@Component
+@DubboService
+public class PatientTeamMealFeeItemDubboRepositoryImpl extends CommonsDubboRepositoryImpl<PatientTeamMealFeeItem, PatientTeamMealCheckItemVo, PatientTeamMealCheckItemDTO, PatientTeamMealCheckItemQuery, PatientTeamMealCheckItemRepository> implements PatientTeamMealCheckItemDubboRepository {
+}
