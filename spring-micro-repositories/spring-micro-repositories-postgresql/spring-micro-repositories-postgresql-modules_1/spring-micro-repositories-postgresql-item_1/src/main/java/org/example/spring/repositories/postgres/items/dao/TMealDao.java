dcd31b8d127703ce490ed5f3c7c360ef9e14fc4f
yuan50697105 <710575900@qq.com>
2021-06-18 13:21:53 +0800

    .

--- spring-micro-repositories/spring-micro-repositories-postgresql/spring-micro-repositories-postgresql-modules_1/spring-micro-repositories-postgresql-item_1/src/main/java/org/example/spring/repositories/postgres/items/dao/TMealDao.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring.repositories.postgres.items.dao;
-
-import org.example.spring.plugins.mybatis.dao.TkBaseDao;
-import org.example.spring.repositories.postgres.items.table.po.TMeal;
-import org.example.spring.repositories.postgres.items.table.query.TMealQuery;
-
-public interface TMealDao extends TkBaseDao<TMeal, TMealQuery> {
-}
--- spring-micro-repositories/spring-micro-repositories-postgresql/spring-micro-repositories-postgresql-modules_2/spring-micro-repositories-postgresql-item_2/src/main/java/org/example/spring/repositories/postgres/items/mapper/TFeeItemQueryMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.spring.repositories.postgres.items.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TFeeItemQueryMapper {
-}
