022c27bd2ce3bbcb0bf08f01b277fa9a34184c2f
yuan50697105 <710575900@qq.com>
2021-06-23 10:07:25 +0800

    .

--- spring-micro-domains-services-provider/spring-micro-domains-services-provider-mysql/spring-micro-domains-services-provider-mysql-modules_2/spring-micro-domains-services-provider-mysql-auth_2/src/main/java/org/example/spring/domains/services/auth/provider/DAccountDubboService.java
+++ spring-micro-domains-services-provider/spring-micro-domains-services-provider-mysql/spring-micro-domains-services-provider-mysql-modules_2/spring-micro-domains-services-provider-mysql-auth_2/src/main/java/org/example/spring/domains/services/auth/provider/DAccountDubboService.java
@@ -1,6 +1,7 @@
 package org.example.spring.domains.services.auth.provider;
 
-import org.example.spring.domains.services.mysql.auth.service.DAccountService;
+
+import org.example.spring.domains.services.auth.mysql.service.DAccountService;
 
 public interface DAccountDubboService extends DAccountService {
 }
