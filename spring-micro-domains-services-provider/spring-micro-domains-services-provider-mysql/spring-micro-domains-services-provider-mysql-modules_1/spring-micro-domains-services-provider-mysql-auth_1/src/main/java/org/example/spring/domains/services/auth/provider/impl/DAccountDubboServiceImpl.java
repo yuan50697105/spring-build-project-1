40f7473f37ff5a2e810f07e337a42a895037cfcf
yuan50697105 <710575900@qq.com>
2021-06-23 10:02:53 +0800

    .

--- spring-micro-domains-services-provider/spring-micro-domains-services-provider-mysql/spring-micro-domains-services-provider-mysql-modules_1/spring-micro-domains-services-provider-mysql-auth_1/src/main/java/org/example/spring/domains/services/auth/provider/impl/DAccountDubboServiceImpl.java
+++ spring-micro-domains-services-provider/spring-micro-domains-services-provider-mysql/spring-micro-domains-services-provider-mysql-modules_1/spring-micro-domains-services-provider-mysql-auth_1/src/main/java/org/example/spring/domains/services/auth/provider/impl/DAccountDubboServiceImpl.java
@@ -1,9 +1,9 @@
 package org.example.spring.domains.services.auth.provider.impl;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.example.spring.domains.services.auth.mysql.handler.UserRegisterHandlerFactory;
+import org.example.spring.domains.services.auth.mysql.service.impl.DAccountServiceImpl;
 import org.example.spring.domains.services.auth.provider.DAccountDubboService;
-import org.example.spring.domains.services.mysql.auth.handler.UserRegisterHandlerFactory;
-import org.example.spring.domains.services.mysql.auth.service.impl.DAccountServiceImpl;
 import org.springframework.stereotype.Service;
 
 @Service
