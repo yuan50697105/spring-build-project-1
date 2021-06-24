385ed096a3261f3696a844c91920ed2dde5c7295
yuan50697105 <710575900@qq.com>
2021-06-24 13:32:13 +0800

    [新增功能](develop): mssql 路径修改

--- spring-micro-repositories-provider/spring-micro-repositories-provider-mssql/spring-micro-repositories-provider-mssql-dubbo-modules/spring-micro-repositories-provider-mssql-bill-dubbo_1/src/main/java/org/example/spring/repositories/clients/auth/mssql/dubbo/BillItemsInfoDubboRepositoryImpl.java
+++ spring-micro-repositories-provider/spring-micro-repositories-provider-mssql/spring-micro-repositories-provider-mssql-dubbo-modules/spring-micro-repositories-provider-mssql-bill-dubbo_1/src/main/java/org/example/spring/repositories/clients/auth/mssql/dubbo/BillItemsInfoDubboRepositoryImpl.java
@@ -3,15 +3,12 @@ package org.example.spring.repositories.clients.auth.mssql.dubbo;
 
 import org.apache.dubbo.config.annotation.DubboService;
 import org.example.spring.repositories.clients.bill.api.BillItemsInfoRepository;
-import org.example.spring.repositories.clients.bill.mssql.api.BillItemsInfoRepositoryImpl;
-import org.example.spring.repositories.clients.bill.mssql.converter.BillItemsInfoRepositoryConverter;
 import org.example.spring.repositories.clients.dubbo.bill.api.BillItemsInfoDubboRepository;
 import org.example.spring.repositories.clients.dubbo.commons.CommonsDubboRepositoryImpl;
 import org.example.spring.repositories.commons.entity.bill.dto.BillItemsInfoDTO;
 import org.example.spring.repositories.commons.entity.bill.po.BillItemsInfo;
 import org.example.spring.repositories.commons.entity.bill.query.BillItemsInfoQuery;
 import org.example.spring.repositories.commons.entity.bill.vo.BillItemsInfoVo;
-import org.example.spring.repositories.mssql.bill.repository.TBillItemsInfoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
--- spring-micro-repositories-provider/spring-micro-repositories-provider-mssql/spring-micro-repositories-provider-mssql-dubbo-modules/spring-micro-repositories-provider-mssql-bill-dubbo_1/src/main/java/org/example/spring/repositories/clients/auth/mssql/dubbo/BillLogDubboRepositoryImpl.java
+++ spring-micro-repositories-provider/spring-micro-repositories-provider-mssql/spring-micro-repositories-provider-mssql-dubbo-modules/spring-micro-repositories-provider-mssql-bill-dubbo_1/src/main/java/org/example/spring/repositories/clients/auth/mssql/dubbo/BillLogDubboRepositoryImpl.java
@@ -3,14 +3,12 @@ package org.example.spring.repositories.clients.auth.mssql.dubbo;
 
 import org.apache.dubbo.config.annotation.DubboService;
 import org.example.spring.repositories.clients.bill.api.BillLogRepository;
-import org.example.spring.repositories.clients.bill.mssql.converter.BillLogRepositoryConverter;
 import org.example.spring.repositories.clients.dubbo.bill.api.BillLogDubboRepository;
 import org.example.spring.repositories.clients.dubbo.commons.CommonsDubboRepositoryImpl;
 import org.example.spring.repositories.commons.entity.bill.dto.BillLogDTO;
 import org.example.spring.repositories.commons.entity.bill.po.BillLog;
 import org.example.spring.repositories.commons.entity.bill.query.BillLogQuery;
 import org.example.spring.repositories.commons.entity.bill.vo.BillLogVo;
-import org.example.spring.repositories.mssql.bill.repository.TBillLogRepository;
 import org.springframework.stereotype.Component;
 
 @Component
