cec64dc1c888e557a27e72eecd91ff781ee770ae
yuan50697105 <710575900@qq.com>
2021-06-26 10:46:13 +0800

    .

--- spring-domains-services-provider/spring-domains-services-provider-mysql/spring-domains-services-provider-mysql-modules_2/spring-domains-services-provider-mysql-auth_2/src/main/java/org/example/spring/domains/services/auth/provider/DAccountDubboService.java
+++
@@ -1,7 +0,0 @@
-package org.example.spring.domains.services.auth.provider;
-
-
-import org.example.spring.domains.services.auth.mysql.service.DAccountService;
-
-public interface DAccountDubboService extends DAccountService {
-}
